c778acc0781389a844d501536c5482ef5a590e36
Martín García <140340907+mitgar14@users.noreply.github.com>
2025-04-06 23:50:16 -0500

    fix (store): made compatible the store module with Docker

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM apache/airflow:2.10.0
-
-USER root
-COPY ./airflow/sources/requirements_docker.txt /requirements_docker.txt
-COPY ./data/spotify_dataset.csv /opt/airflow/data/spotify_dataset.csv
-
-USER airflow
-RUN pip install --no-cache-dir -r /requirements_docker.txt
--- eliminar_pycache.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Buscando y eliminando todos los directorios __pycache__..."
-
-Get-ChildItem -Path . -Recurse -Directory -Filter "__pycache__" | ForEach-Object {
-    Write-Host "Eliminando:" $_.FullName
-    Remove-Item $_.FullName -Recurse -Force
-}
-
-Write-Host "Eliminación completada."
