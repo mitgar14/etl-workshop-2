bd6420fc2090ad62379c3e98b313bba0e5d55404
Martín García <140340907+mitgar14@users.noreply.github.com>
2025-04-06 23:52:06 -0500

    ps1: improving pycache script

--- eliminar_pycache.ps1
+++ eliminar_pycache.ps1
@@ -1,7 +1,6 @@
 Write-Host "Buscando y eliminando directorios __pycache__ (excepto los de venv)..."
 
 Get-ChildItem -Path . -Recurse -Directory -Filter "__pycache__" | Where-Object {
-    # Excluir cualquier carpeta que tenga 'venv' en su ruta
     $_.FullName -notmatch "\\venv\\"
 } | ForEach-Object {
     Write-Host "Eliminando:" $_.FullName
